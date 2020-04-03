639d505a9c7f509602a54eb1b0f07c932df2dd18
Adalberto Cajueiro <adalberto.cajueiro@gmail.com>
2020-04-03 16:38:38 -0300

    Adjustment in the start script to accept calls from any IP.

--- start-server.sh
+++ start-server.sh
@@ -1,7 +1,6 @@
-#!/bin/sh
-cd /home/ubuntu/
-source .bashrc
+#!/bin/bash
+export PATH=$PATH:/home/ubuntu/anaconda3/bin
 cd /home/ubuntu/covid-back
 export FLASK_APP=covid-server.py
 export FLASK_RUN_PORT=8080
-flask run
+flask run --host=0.0.0.0 &
